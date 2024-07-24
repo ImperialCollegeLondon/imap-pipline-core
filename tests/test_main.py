9b7f70ddb6857e0732887b7fc36a5ec2f2df2147
Michele Facchinelli <m.facchinelli@imperial.ac.uk>
2024-07-24 13:07:14 +0000

    test: add simple test for HK processing

--- tests/test_main.py
+++ tests/test_main.py
@@ -35,7 +35,7 @@ def test_process_with_valid_config_does_not_error(tidyDataFolders):
             "--config",
             "config.yml",
             "--file",
-            "solo_L2_mag-rtn-ll-internal_20240210_V00.cdf",
+            "imap_mag_l1a_norm-mago_20250502_v000.cdf",
         ],
     )
 
@@ -43,3 +43,21 @@ def test_process_with_valid_config_does_not_error(tidyDataFolders):
     assert result.exit_code == 0
     # check that file output/result.cdf exists
     assert Path("output/result.cdf").exists()
+
+
+def test_process_hk_ccsds(tidyDataFolders):
+    result = runner.invoke(
+        app,
+        [
+            "process",
+            "--config",
+            "tests/config/hk.yaml",
+            "--file",
+            "MAG_HSK_PW.pkts",
+        ],
+    )
+
+    print("\n" + str(result.stdout))
+    assert result.exit_code == 0
+    # check that file output/result.cdf exists
+    assert Path("output/result.csv").exists()
