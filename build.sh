ee9aacbcfad96780a4bf33dba7a9ca0ad6dc024c
Alastair Crabtree <a.crabtree@imperial.ac.uk>
2024-07-25 13:48:42 +0000

    feat: replace black/isort/flake8 with ruff

--- build.sh
+++ build.sh
@@ -6,20 +6,14 @@ set -e
 # what version is this?
 python3 --version
 
+# load the current python virtual environment - assumes you have already probably run "poetry shell" or are calling from build-python-versions.sh
+source .venv/bin/activate
+
 # restore dependencies
 poetry install
 
-# load the python virtual environment
-source .venv/bin/activate
-
-# tidy up fomatting
-poetry run isort src tests
-poetry run black src
-
-# check syntax
-poetry run flake8
+# tidy up fomatting and check syntax
+poetry run ruff check --fix
 
 # execute unit tests with code coverage
 poetry run pytest -s --cov-config=.coveragerc --cov=src --cov-append --cov-report=xml --cov-report term-missing --cov-report=html --junitxml=test-results.xml tests
-
-
