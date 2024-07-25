90487cf01283cffec27fdf0006198fec56ed9725
Michele Facchinelli <19731497+mfacchinelli@users.noreply.github.com>
2024-07-25 16:05:15 +0100

    feat: Add decommutation of HK (#8)
    * build: add tools for consistent software development
    * chore: update pre-commit hooks
    * fix: issue with devcontainer setup
    * fix: failing tests due to replaced type, remove type replacement
    * build: add dependencies for HK processing
    * test: add IMAP test data
    * feat: add support for processing HK packets
    * test: add simple test for HK processing
    * doc: add info for generating XTCE file
    * dev: add more extensions for devcontainer, clear build output before running again
    * fix: only return first ApID
    * dev: add known words to spell check exceptions
    * fix: return input file as default, if no data found
    * fix: apply PR feedback
    * task: re-add packet header, as provides useful information, such as sequence counter
    
    ---------
    
    Co-authored-by: Alastair Crabtree <a.crabtree@imperial.ac.uk>

--- src/imapProcessing.py
+++ src/imapProcessing.py
@@ -1,5 +1,108 @@
+import abc
+import collections
+import logging
+import re
 from pathlib import Path
 
+import xarray as xr
+from space_packet_parser import parser, xtcedef
 
-def doSomething(file: Path) -> Path:
-    return file
+from src import appConfig, appUtils
+
+
+class FileProcessor(abc.ABC):
+    """Interface for IMAP processing."""
+
+    @abc.abstractmethod
+    def initialize(self, config: appConfig.AppConfig) -> None:
+        pass
+
+    @abc.abstractmethod
+    def process(self, file: Path) -> Path:
+        pass
+
+
+class ScienceProcessor(FileProcessor):
+    def initialize(self, config: appConfig.AppConfig) -> None:
+        pass
+
+    def process(self, file: Path) -> Path:
+        return file
+
+
+class HKProcessor(FileProcessor):
+    xtcePacketDefinition: Path
+
+    def initialize(self, config: appConfig.AppConfig) -> None:
+        self.xtcePacketDefinition = config.packet_definition.hk
+
+    def process(self, file: Path) -> Path:
+        """Process HK with XTCE tools and create CSV file."""
+
+        # Extract data from binary file.
+        dataDict: dict[int, dict] = dict()
+
+        packetDefinition = xtcedef.XtcePacketDefinition(self.xtcePacketDefinition)
+        packetParser = parser.PacketParser(packetDefinition)
+
+        with open(file, "rb") as binaryData:
+            packetGenerator = packetParser.generator(binaryData)
+
+            for packet in packetGenerator:
+                apid = packet.header["PKT_APID"].raw_value
+                dataDict.setdefault(apid, collections.defaultdict(list))
+
+                packetContent = packet.data | packet.header
+
+                for key, value in packetContent.items():
+                    dataDict[apid][key].append(value.derived_value or value.raw_value)
+
+        # Convert data to xarray datasets.
+        datasetDict = {}
+
+        for apid, data in dataDict.items():
+            time_key = next(iter(data.keys()))
+            time_data = appUtils.met_to_j2000ns(data[time_key])
+
+            ds = xr.Dataset(
+                {
+                    re.sub(r"^mag_hsk_[a-zA-Z]+_", "", key.lower()): ("epoch", val)
+                    for key, val in data.items()
+                },
+                coords={"epoch": time_data},
+            )
+            ds = ds.sortby("epoch")
+
+            datasetDict[apid] = ds
+
+        # Write CSV files.
+        for apid, dataset in datasetDict.items():
+            csvFile = file.with_suffix(".csv")
+            dataset.to_dataframe().to_csv(csvFile)
+
+            # TODO: What about the other ApIDs?
+            return csvFile
+
+        # No data found.
+        return file
+
+
+class UnknownProcessor(FileProcessor):
+    def initialize(self, config: appConfig.AppConfig) -> None:
+        pass
+
+    def process(self, file: Path) -> Path:
+        return file
+
+
+def dispatchFile(file: Path) -> FileProcessor:
+    match file.suffix:
+        case ".cdf":
+            logging.info(f"File {file} contains science.")
+            return ScienceProcessor()
+        case ".pkts" | ".bin":
+            logging.info(f"File {file} contains HK.")
+            return HKProcessor()
+        case _:
+            logging.info(f"File {file} contains unknown data.")
+            return UnknownProcessor()
--- tests/test_main.py
+++ tests/test_main.py
@@ -34,7 +34,7 @@ def test_process_with_valid_config_does_not_error(tidyDataFolders):
             "process",
             "--config",
             "config.yml",
-            "solo_L2_mag-rtn-ll-internal_20240210_V00.cdf",
+            "imap_mag_l1a_norm-mago_20250502_v000.cdf",
         ],
     )
 
@@ -42,3 +42,35 @@ def test_process_with_valid_config_does_not_error(tidyDataFolders):
     assert result.exit_code == 0
     # check that file output/result.cdf exists
     assert Path("output/result.cdf").exists()
+
+
+def test_process_with_binary_hk_converts_to_csv(tidyDataFolders):
+    # Set up.
+    expectedHeader = "epoch,shcoarse,pus_spare1,pus_version,pus_spare2,pus_stype,pus_ssubtype,hk_strucid,p1v5v,p1v8v,p3v3v,p2v5v,p8v,n8v,icu_temp,p2v4v,p1v5i,p1v8i,p3v3i,p2v5i,p8vi,n8vi,fob_temp,fib_temp,magosatflagx,magosatflagy,magosatflagz,magisatflagx,magisatflagy,magisatflagz,spare1,magorange,magirange,spare2,magitfmisscnt,version,type,sec_hdr_flg,pkt_apid,seq_flgs,src_seq_ctr,pkt_len\n"
+    expectedFirstLine = "799424368184000000,483848304,0,1,0,3,25,3,1.52370834,1.82973516,3.3652049479999997,2.54942028,9.735992639,-9.7267671632,19.470153600000003,2.36297684,423.7578925213,18.436028516,116.40531765999998,87.2015252,119.75070000000001,90.32580000000002,19.640128302955475,19.482131117873905,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1063,3,0,43\n"
+    expectedLastLine = "799437851184000000,483861787,0,1,0,3,25,3,1.52370834,1.82973516,3.3652049479999997,2.54942028,9.555648769,-9.5531674296,26.019506700000022,2.3559926719999997,419.9364473837,31.800489164000002,131.13964636,92.94935734500001,193.83599999999998,154.8802,25.938177593750083,25.628958683022688,0,0,0,0,0,0,0,3,3,0,0,0,0,1,1063,3,495,43\n"
+    expectedNumRows = 1335
+
+    # Execute.
+    result = runner.invoke(
+        app,
+        [
+            "process",
+            "--config",
+            "tests/config/hk.yaml",
+            "MAG_HSK_PW.pkts",
+        ],
+    )
+
+    print("\n" + str(result.stdout))
+
+    # Verify.
+    assert result.exit_code == 0
+    assert Path("output/result.csv").exists()
+
+    with open("output/result.csv") as f:
+        lines = f.readlines()
+        assert expectedHeader == lines[0]
+        assert expectedFirstLine == lines[1]
+        assert expectedLastLine == lines[-1]
+        assert expectedNumRows == len(lines)
