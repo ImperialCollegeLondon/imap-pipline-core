4dc9b0cfa54a24d1c75f102ad70386b0f1eeafe9
mhairifin <m.finlayson@imperial.ac.uk>
2024-07-29 11:18:16 +0100

    resolve typing issues

--- src/mag_toolkit/calibration/MatlabWrapper.py
+++ src/mag_toolkit/calibration/MatlabWrapper.py
@@ -1,16 +1,38 @@
 from datetime import datetime
 
+import numpy as np
+from pydantic import BaseModel
 
-def simulateSpinAxisCalibration(xarray):
+
+class BasicCalibration(BaseModel):
+    timestamps: list[datetime]
+    x_offsets: list[float]
+    y_offsets: list[float]
+    z_offsets: list[float]
+
+
+def simulateSpinAxisCalibration(xarray) -> BasicCalibration:
+    # TODO: Transfer to MATLAB to get spin axis offsets
     timestamps = [datetime(2022, 3, 3)]
     offsets = [3.256]
 
-    return (timestamps, offsets)
+    return BasicCalibration(
+        timestamps=timestamps,
+        x_offsets=np.zeros(len(offsets)),
+        y_offsets=np.zeros(len(offsets)),
+        z_offsets=offsets,
+    )
 
 
 def simulateSpinPlaneCalibration(xarray):
+    # TODO: Transfer to MATLAB to get spin plane offsets
     timestamps = [datetime(2022, 3, 3)]
     offsets_x = [3.256]
     offsets_y = [2.76]
 
-    return (timestamps, offsets_x, offsets_y)
+    return BasicCalibration(
+        timestamps=timestamps,
+        x_offsets=offsets_x,
+        y_offsets=offsets_y,
+        z_offsets=np.zeros(len(offsets_x)),
+    )
